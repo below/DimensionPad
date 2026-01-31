a150f8d65858b5983e308a500f72434a265b6193
Alexander von Below <alex@vonbelow.com>
2026-01-31 13:56:40 +0100

    Split the code up in files for better maintenance

--- Sources/DimensionPad/DimensionPad.swift
+++ Sources/DimensionPad/DimensionPad.swift
@@ -2,116 +2,6 @@ import Foundation
 import Combine
 import IOKit.hid
 
-/// Type of LEGO Dimensions NFC tag payload.
-public enum TagType {
-    case character
-    case vehicle
-    case unknown
-}
-
-/// Basic decoded tag information.
-public struct TagInfo {
-    public let type: TagType
-    public let id: Int
-    public let signature: String
-}
-
-/// Published state for a single pad.
-public struct PadState: Sendable {
-    public let present: Bool
-    public let uid: String?
-    public let characterID: Int?
-    public let name: String?
-
-    /// Creates a new pad state.
-    public init(present: Bool, uid: String?, characterID: Int?, name: String?) {
-        self.present = present
-        self.uid = uid
-        self.characterID = characterID
-        self.name = name
-    }
-}
-
-public enum Pad: UInt8, Sendable {
-    case all = 0
-    case center = 1
-    case left = 2
-    case right = 3
-    
-}
-
-/// Flash configuration for a pad.
-public struct FlashPad: Sendable {
-    /// Whether the pad is enabled (used by flashAll).
-    public var enabled: Bool
-    /// Ticks to stay on. Higher is longer.
-    public var tickOn: UInt8
-    /// Ticks to stay off. Higher is longer.
-    public var tickOff: UInt8
-    /// Number of pulses; 0xFF means forever.
-    public var tickCount: UInt8
-    /// Red component.
-    public var r: UInt8
-    /// Green component.
-    public var g: UInt8
-    /// Blue component.
-    public var b: UInt8
-
-    public init(enabled: Bool = true, tickOn: UInt8, tickOff: UInt8, tickCount: UInt8, r: UInt8, g: UInt8, b: UInt8) {
-        self.enabled = enabled
-        self.tickOn = tickOn
-        self.tickOff = tickOff
-        self.tickCount = tickCount
-        self.r = r
-        self.g = g
-        self.b = b
-    }
-    
-    /// Convenience for flashing forever (tickCount = 0xFF).
-    public static func forever(enabled: Bool = true, tickOn: UInt8, tickOff: UInt8, r: UInt8, g: UInt8, b: UInt8) -> FlashPad {
-        FlashPad(enabled: enabled, tickOn: tickOn, tickOff: tickOff, tickCount: 0xFF, r: r, g: g, b: b)
-    }
-}
-
-/// Fade configuration for a pad.
-public struct FadePad: Sendable {
-    /// Whether the pad is enabled (used by fadeAll).
-    public var enabled: Bool
-    /// Ticks to fade. Higher is longer.
-    public var tickTime: UInt8
-    /// Tick count. Even stops on old color, odd on new color. 0 is never.
-    public var tickCount: UInt8
-    /// Red component.
-    public var r: UInt8
-    /// Green component.
-    public var g: UInt8
-    /// Blue component.
-    public var b: UInt8
-
-    public init(enabled: Bool = true, tickTime: UInt8, tickCount: UInt8, r: UInt8, g: UInt8, b: UInt8) {
-        self.enabled = enabled
-        self.tickTime = tickTime
-        self.tickCount = tickCount
-        self.r = r
-        self.g = g
-        self.b = b
-    }
-}
-
-/// Event emitted when a tag is added or removed.
-public struct TagEvent: Sendable {
-    public enum Action: Sendable {
-        case add
-        case remove
-    }
-
-    public let action: Action
-    public let pad: Pad
-    public let signature: String
-    public let index: UInt8
-    public let uid: [UInt8]
-}
-
 /// HID-backed interface to the LEGO Dimensions Toy Pad.
 @MainActor
 public final class DimensionPad {
@@ -151,7 +41,7 @@ public final class DimensionPad {
     private var inputReport = [UInt8](repeating: 0, count: 32)
 
     internal var msgCounter: UInt8 = 0x01
-    private func nextMsgAvailable() throws -> UInt8 {
+    func nextMsgAvailable() throws -> UInt8 {
         for _ in 0..<256 {
             let m = msgCounter
             msgCounter &+= 1
@@ -233,98 +123,6 @@ public final class DimensionPad {
         case .unknown:
             return TagInfo(type: .unknown, id: 0, signature: tag.signature)
         }
-    }
-
-    /// Set the RGB LED color for a pad, or all pads
-    public func setColor(pad: Pad, r: UInt8, g: UInt8, b: UInt8) async throws {
-        guard let dev = self.device else { throw ToyPadReadError.notConnected }
-        let msg = try nextMsgAvailable()
-        let cmd = createSetColorCommand(msg: msg, pad: pad.rawValue, r: r, g: g, b: b)
-        sendCommand(dev, cmd)
-    }
-    
-    /// Flash a single pad with a color pattern.
-    public func flash(pad: Pad, flashPad: FlashPad) async throws {
-        guard let dev = self.device else { throw ToyPadReadError.notConnected }
-        let msg = try nextMsgAvailable()
-        let cmd = createFlashCommand(
-            msg: msg,
-            pad: pad.rawValue,
-            tickOn: flashPad.tickOn,
-            tickOff: flashPad.tickOff,
-            tickCount: flashPad.tickCount,
-            r: flashPad.r,
-            g: flashPad.g,
-            b: flashPad.b
-        )
-        sendCommand(dev, cmd)
-    }
-    
-    /// Flash a single pad with explicit parameters.
-    public func flash(
-        pad: Pad,
-        tickOn: UInt8,
-        tickOff: UInt8,
-        tickCount: UInt8,
-        r: UInt8,
-        g: UInt8,
-        b: UInt8
-    ) async throws {
-        let flashPad = FlashPad(tickOn: tickOn, tickOff: tickOff, tickCount: tickCount, r: r, g: g, b: b)
-        try await flash(pad: pad, flashPad: flashPad)
-    }
-    
-    /// Flash all pads with independent patterns.
-    public func flashAll(center: FlashPad, left: FlashPad, right: FlashPad) async throws {
-        guard let dev = self.device else { throw ToyPadReadError.notConnected }
-        let msg = try nextMsgAvailable()
-        let cmd = createFlashAllCommand(msg: msg, center: center, left: left, right: right)
-        sendCommand(dev, cmd)
-    }
-    
-    /// Fade a single pad with a color transition.
-    public func fade(pad: Pad, fadePad: FadePad) async throws {
-        guard let dev = self.device else { throw ToyPadReadError.notConnected }
-        let msg = try nextMsgAvailable()
-        let cmd = createFadeCommand(
-            msg: msg,
-            pad: pad.rawValue,
-            tickTime: fadePad.tickTime,
-            tickCount: fadePad.tickCount,
-            r: fadePad.r,
-            g: fadePad.g,
-            b: fadePad.b
-        )
-        sendCommand(dev, cmd)
-    }
-    
-    /// Fade a single pad with explicit parameters.
-    public func fade(
-        pad: Pad,
-        tickTime: UInt8,
-        tickCount: UInt8,
-        r: UInt8,
-        g: UInt8,
-        b: UInt8
-    ) async throws {
-        let fadePad = FadePad(tickTime: tickTime, tickCount: tickCount, r: r, g: g, b: b)
-        try await fade(pad: pad, fadePad: fadePad)
-    }
-    
-    /// Fade all pads with independent patterns.
-    public func fadeAll(center: FadePad, left: FadePad, right: FadePad) async throws {
-        guard let dev = self.device else { throw ToyPadReadError.notConnected }
-        let msg = try nextMsgAvailable()
-        let cmd = createFadeAllCommand(msg: msg, center: center, left: left, right: right)
-        sendCommand(dev, cmd)
-    }
-    
-    /// Fade to random colors on a pad.
-    public func fadeRandom(pad: Pad, tickTime: UInt8, tickCount: UInt8) async throws {
-        guard let dev = self.device else { throw ToyPadReadError.notConnected }
-        let msg = try nextMsgAvailable()
-        let cmd = createFadeRandomCommand(msg: msg, pad: pad.rawValue, tickTime: tickTime, tickCount: tickCount)
-        sendCommand(dev, cmd)
     }
 
     private func resolveNameForPad(pad: Pad, signature: String) async {
@@ -641,7 +439,7 @@ public final class DimensionPad {
         sendCommand(dev, [0x55, 0x06, 0xC0, 0x02, pad.rawValue, r, g, b])
     }
 
-    private func sendCommand(_ dev: IOHIDDevice, _ cmd: [UInt8]) {
+    func sendCommand(_ dev: IOHIDDevice, _ cmd: [UInt8]) {
         var message = cmd
         message.append(checksum(cmd))        // add checksum byte
 
@@ -672,72 +470,6 @@ public final class DimensionPad {
         [0x55, 0x04, 0xD2, msg, index, page]
     }
 
-    private func createSetColorCommand(msg: UInt8, pad: UInt8, r: UInt8, g: UInt8, b: UInt8) -> [UInt8] {
-        [0x55, 0x06, 0xC0, msg, pad, r, g, b]
-    }
-    
-    private func createFlashCommand(
-        msg: UInt8,
-        pad: UInt8,
-        tickOn: UInt8,
-        tickOff: UInt8,
-        tickCount: UInt8,
-        r: UInt8,
-        g: UInt8,
-        b: UInt8
-    ) -> [UInt8] {
-        // 0x55 0x09 0xC3 <msg> <pad> <tickOn> <tickOff> <tickCount> <R> <G> <B>
-        [0x55, 0x09, 0xC3, msg, pad, tickOn, tickOff, tickCount, r, g, b]
-    }
-    
-    private func createFlashAllCommand(
-        msg: UInt8,
-        center: FlashPad,
-        left: FlashPad,
-        right: FlashPad
-    ) -> [UInt8] {
-        // 0x55 0x17 0xC7 <msg> then 3x: <enabled> <tickOn> <tickOff> <tickCount> <R> <G> <B>
-        [
-            0x55, 0x17, 0xC7, msg,
-            center.enabled ? 0x01 : 0x00, center.tickOn, center.tickOff, center.tickCount, center.r, center.g, center.b,
-            left.enabled ? 0x01 : 0x00, left.tickOn, left.tickOff, left.tickCount, left.r, left.g, left.b,
-            right.enabled ? 0x01 : 0x00, right.tickOn, right.tickOff, right.tickCount, right.r, right.g, right.b
-        ]
-    }
-    
-    private func createFadeCommand(
-        msg: UInt8,
-        pad: UInt8,
-        tickTime: UInt8,
-        tickCount: UInt8,
-        r: UInt8,
-        g: UInt8,
-        b: UInt8
-    ) -> [UInt8] {
-        // 0x55 0x08 0xC2 <msg> <pad> <tickTime> <tickCount> <R> <G> <B>
-        [0x55, 0x08, 0xC2, msg, pad, tickTime, tickCount, r, g, b]
-    }
-    
-    private func createFadeAllCommand(
-        msg: UInt8,
-        center: FadePad,
-        left: FadePad,
-        right: FadePad
-    ) -> [UInt8] {
-        // 0x55 0x14 0xC6 <msg> then 3x: <enabled> <tickTime> <tickCount> <R> <G> <B>
-        [
-            0x55, 0x14, 0xC6, msg,
-            center.enabled ? 0x01 : 0x00, center.tickTime, center.tickCount, center.r, center.g, center.b,
-            left.enabled ? 0x01 : 0x00, left.tickTime, left.tickCount, left.r, left.g, left.b,
-            right.enabled ? 0x01 : 0x00, right.tickTime, right.tickCount, right.r, right.g, right.b
-        ]
-    }
-    
-    private func createFadeRandomCommand(msg: UInt8, pad: UInt8, tickTime: UInt8, tickCount: UInt8) -> [UInt8] {
-        // 0x55 0x05 0xC4 <msg> <pad> <tickTime> <tickCount>
-        [0x55, 0x05, 0xC4, msg, pad, tickTime, tickCount]
-    }
-    
     // MARK: General Utilities
     
     private func hex(_ bytes: [UInt8]) -> String {
